1581cf4d3306aaeac77e0724735a34a12776fc90
Jonathon Paul Guillermo Tordilla <156870738+jpgtordilla@users.noreply.github.com>
2024-07-10 16:35:06 -0700

    Finished peak minima finder and separation by charges, but have not tested any of the new methods, and all methods are failing since no data is fetched

--- archive-analysis/lcls-tools-plots/lcls_tools_plotter.py
+++ archive-analysis/lcls-tools-plots/lcls_tools_plotter.py
@@ -7,9 +7,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from scipy.signal import find_peaks
+import re
 
 class LclsToolsPlotter(): 
     def __init__(self): 
+        self.charge_vals = [] # FILL WITH CHARGE VALUES
         return
     
     """Create and return a DataFrame given a PV and start/end date"""
@@ -94,7 +96,7 @@ class LclsToolsPlotter():
         return 
     
     """Grid plotter helper function for megaplots"""
-    def plot_mega(self, ax, df_name, i: int, j: int, df_ind: int): 
+    def plot_mega(self, ax, df_name, i: int, j: int): 
         ax[i, j].scatter(df_name["timestamps"], df_name[df_name.columns[1]], s=10)
         ax[i, j].xaxis.set_major_locator(ticker.LinearLocator(3))
         ax[i, j].set_xlabel("Timestamp")
@@ -106,7 +108,7 @@ class LclsToolsPlotter():
         return
     
     """Helper method to return a DataFrame of peaks over time given lists for peak indices and peak heights"""
-    def create_peaks_df(self, df_curr, pv_name, peak_indices, peak_heights): 
+    def create_peaks_df(self, df_curr: any, pv_name: str, peak_indices: list, peak_heights: list): 
         all_timestamps_list = df_curr["timestamps"].tolist()
         peak_timestamps_list = [all_timestamps_list[x] for x in peak_indices]
         peak_df_data = {"timestamps": peak_timestamps_list, pv_name: peak_heights}
@@ -195,6 +197,55 @@ class LclsToolsPlotter():
         plt.show()
         return self.return_peaks(pv_list, start, end, peak_height, peak_spacing)
     
+    """Returns list of peak DataFrames from a DataFrame"""
+    def return_peaks_from_df(self, df: any, peak_height: float, peak_spacing: float): 
+        peak_df_list = []
+        for pv_name in df.columns: 
+            if pv_name == "timestamps": 
+                continue
+            y_axis = df[pv_name] # get the column that corresponds with the pv values
+            peak_indices, peak_heights = find_peaks(y_axis, height=peak_height, distance=peak_spacing) # use scipy to generate lists of peak indices and their heights
+            peak_indices_2, peak_heights_2 = find_peaks(-y_axis, height=peak_height, distance=peak_spacing) # find minima
+            # combine lists of maxima and minima
+            all_peak_indices = np.concatenate((peak_indices, peak_indices_2), axis=None)
+            all_peak_heights = np.concatenate((peak_heights["peak_heights"], peak_heights_2["peak_heights"]), axis=None)
+            # return a DataFrame with timestamps and peaks
+            peak_df = self.create_peaks_df(df, pv_name, all_peak_indices.tolist(), all_peak_heights.tolist())
+            peak_df_list.append(peak_df)
+        return peak_df_list
+    
+    """Plot peaks from a DataFrame"""
+    def plot_peaks_from_df(self, df: any, pv_y: str, pv_x: str, peak_height: float, peak_spacing: float, show_plots=True): 
+         # plot peaks based on parameters
+        fig, ax = plt.subplots(2, 1, figsize=(12, 8))
+        plt.subplots_adjust(wspace=0.4, hspace=0.3)
+        y = df[pv_y]
+        x = df[pv_x]
+        all_peaks, peak_heights = find_peaks(y, height=peak_height, distance=peak_spacing) # use scipy to generate lists of peak indices and their heights
+        ax[0].scatter(df[pv_x], df[pv_y]) 
+        # plot only peak points with x axis showing index corresponding to the returned dict
+        ax[1].scatter(all_peaks, y[all_peaks])
+        # match limits (approximately) with the first plot
+        ax[1].set_xlim(0, len(x))
+        # set labels
+        for i in range(2): 
+            ax[i].set_xlabel(f"{pv_x}")
+            ax[i].set_ylabel(f"{pv_y}")
+            ax[i].xaxis.set_major_locator(ticker.LinearLocator(5))
+            ax[i].yaxis.set_major_locator(ticker.LinearLocator(5))
+            # set significant figures
+            x_ticks = [np.format_float_scientific(i, precision=3, min_digits=2) for i in x] # convert to scientific notation
+            y_ticks = [np.format_float_scientific(i, precision=3, min_digits=2) for i in y]
+            ax[i].set_xticklabels(x_ticks, fontsize=10) # set tick labels
+            ax[i].set_yticklabels(y_ticks, fontsize=10) # set tick labels
+            ax[i].set_title(f"{pv_y} vs. {pv_x}")
+        
+        if show_plots: 
+            plt.show()
+
+        return self.return_peaks_from_df(df, peak_height, peak_spacing)
+
+    
     """Given two PVs, return a single DataFrame with matching and aligned timestamps"""
     def create_correlation_df(self, pv_one: str, pv_two: str, start: str, end: str): 
         df_one = self.create_df(pv_one, start, end)
@@ -244,7 +295,7 @@ class LclsToolsPlotter():
             return IndexError("Could not plot PVs because an empty list was given")
         # if the length of pv_list is only 1, call the non-megaplot correlation function 
         if len(pv_list) == 1: 
-            self.plot_correlation(pv_list[0], pv_y, start, end)
+            self.plot_correlation([pv_list[0]], pv_y, start, end)
             return
         # want to create a grid of subplots
         col_len = len(pv_list)
@@ -295,6 +346,47 @@ class LclsToolsPlotter():
         x_col = pv_list[pv_ind]
         y_col = pv_y
         df = self.create_correlation_df(pv_list[pv_ind], pv_y, start, end)
+        ax[i, j].scatter(df[x_col], df[y_col], s=10)
+        # tick settings
+        x_ticks = [np.format_float_scientific(i, precision=3, min_digits=2) for i in df[x_col]] # convert to scientific notation
+        y_ticks = [np.format_float_scientific(i, precision=3, min_digits=2) for i in df[y_col]]
+        ax[i, j].set_xticklabels(x_ticks, fontsize=5)
+        ax[i, j].set_yticklabels(y_ticks, fontsize=5)
+        ax[i, j].xaxis.set_major_locator(ticker.LinearLocator(5))
+        ax[i, j].yaxis.set_major_locator(ticker.LinearLocator(5))
+        # set labels
+        ax[i, j].set_xlabel(f"{x_col}")
+        ax[i, j].set_ylabel(f"{y_col}") 
+        ax[i, j].set_title(f"{y_col} vs. {x_col}", {'fontsize': 10})
+        return
+    
+    """Megaplotter that creates a correlation between two PVs with respect to a charge PV, for each charge value"""
+    def megaplot_correlation_charge_separated(self, pv_x: str, pv_y: str, pv_charge: str, start: str, end: str, tolerance=0.05, w_margin=0.4, h_margin=1): 
+        # want to create a grid of subplots
+        col_len = len(self.charge_vals) 
+        dim = int(np.round(np.sqrt(col_len))) # dimension of the grid
+
+        fig, ax = plt.subplots(dim + 1, dim, figsize=(17, 15)) # add an extra row to account for rounding 
+        fig.suptitle("PV Correlations")
+        plt.subplots_adjust(wspace=w_margin, hspace=h_margin) # can set if needed using optional parameters
+        charge_ind = 0 # track index of the which pv is being plotted
+        
+        # iterate through the grid 
+        for i in range(0, dim + 1): # for every row in the grid
+            for j in range(0, dim): # for every column in the grid
+                if charge_ind >= len(self.charge_vals): 
+                    break
+                self.plot_correl_charge_mega(ax, pv_x, pv_y, pv_charge, i, j, charge_ind, start, end, tolerance)
+                charge_ind += 1 # go to the next df in the list
+        plt.show()
+        return
+
+    """Helper method for megaplotter between two PVs with respect to charge"""
+    def plot_correl_charge_mega(self, ax, pv_x, pv_y, pv_charge, i, j, charge_ind, start, end, tolerance): 
+        x_col = pv_x
+        y_col = pv_y
+        charge = self.charge_vals[charge_ind]
+        df = self.create_correlation_charge_df(pv_charge, pv_x, pv_y, start, end, charge, tolerance) # create correlation between pv_x and pv_y for each charge
         ax[i, j].scatter(df[x_col], df[y_col], s=10)
         # tick settings
         x_ticks = [np.format_float_scientific(i, precision=3, min_digits=2) for i in df[x_col]] # convert to scientific notation
