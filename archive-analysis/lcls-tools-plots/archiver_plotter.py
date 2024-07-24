c1546b745666d57374d5b5ef5a09147523e7c974
Jonathon Paul Guillermo Tordilla <156870738+jpgtordilla@users.noreply.github.com>
2024-07-24 12:13:32 -0700

    almost done with archiver_plotter, still need to test, write docstrings, etc.

--- archive-analysis/lcls-tools-plots/archiver_plotter.py
+++ archive-analysis/lcls-tools-plots/archiver_plotter.py
@@ -1,18 +1,20 @@
 import sys
-sys.path.append('/archive-analysis/lcls-tools-plots/lcls_tools')  # TODO: change to your path to lcls_tools
+# TODO: change to relative path within lcls_tools
+sys.path.append(
+    '/Users/jonathontordilla/Desktop/hombom24/archive-analysis/lcls-tools-plots/lcls_tools')
 import common.data_analysis.archiver as arch  # type: ignore
 from datetime import datetime
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker 
+import matplotlib.ticker as ticker
 
 
 class ArchiverPlotter:
     def __init__(self):
         return
 
-    """HELPER METHODS FOR PLOTTING""" 
+    """HELPER METHODS FOR PLOTTING"""
 
     def create_df(self, pv_str: str, start: str, end: str) -> pd.DataFrame:
         """Create and return a DataFrame given a PV and start/end date.
@@ -37,149 +39,215 @@ class ArchiverPlotter:
         pv_clean_timestamps = [pv_timestamps[i].strftime('%m/%d/%Y %H:%M:%S') for i in
                                range(len(pv_timestamps))]  # clean and reformat timestamps from the dict
         return pd.DataFrame({"timestamps": pv_clean_timestamps, pv_str: pv_values})  # create df with columns
-    
+
     def create_correlation_df(self, df_x: pd.DataFrame, df_y: pd.DataFrame) -> pd.DataFrame:
         """Given two DataFrames of PVs, return a single DataFrame with matching and aligned timestamps.
 
         :param df_y: The name of the PV or the DataFrame that will be plotted on the y-axis.
         :param df_x: The name of the PV that will be plotted on the x-axis.
         """
-        
+
         return pd.merge(df_y, df_x, on="timestamps")  # merge DataFrames on equal timestamp strings
 
-    @staticmethod 
-    def get_smart_ticks(timestamps: pd.Series) -> dict{str: float}: 
-        """Returns a list of dictionary with float values and a string describing what they represent."""
-        # TODO: define smart ticks function 
-        pass 
-
-        
     """PLOTTING METHODS"""
-    def plot_pv_over_time(self, 
-                          df_list: list[pd.DataFrame], 
-                          start: str, 
-                          end: str, 
-                          width=10: int, 
-                          height=7: int, 
-                          xlabel_bottom="Timestamp": str, 
-                          xlabel_top=None: str, 
-                          ylabel_left="PV": str, 
-                          ylabel_right=None: str, 
-                          xlabel_color_botom="black": str, 
-                          xlabel_color_top=None: str, 
-                          ylabel_color_left="black": str, 
-                          ylabel_color_right=None: str, 
-                          label_font="Helvetica": str,  
-                          pv_colors=["red", "green", "blue", "black"]: list[str], 
-                          line_types=["-", "-.", "--", "--x"]: list[str], 
-                          is_scatter=False, 
-                          marker_size=3, 
-                          labels=None: list[str], 
-                          smart_ticks=False: boolean) -> None:
+
+    def plot_pv_over_time(self,
+                          df_list: list[pd.DataFrame],
+                          width=10,
+                          height=7,
+                          xlabel_bottom="Timestamp",
+                          ylabel_left="PV",
+                          xlabel_color="black",
+                          ylabel_color="black",
+                          title_color="black",
+                          label_font="Helvetica",
+                          pv_colors=("tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple"),
+                          line_types=("solid", "dashed", "dashdot", "dotted"),
+                          marker_types=("x", ".", "^", "s", "p", "*"),
+                          is_scatter=False,
+                          is_marker=False,
+                          marker_size=5,
+                          pv_labels=None,
+                          num_ticks=7,
+                          smart_title=False) -> None:
         """Plots a nonempty list of PVs over time.
 
-        :param df_list: A list of DataFrames from which to plot the PVs. 
-        :param start: The start date of the plot in YYYY/MM/DD HH:MM:SS format.
-        :param end: The end date of the plot in YYYY/MM/DD HH:MM:SS format.
+        :param df_list: A list of DataFrames from which to plot the PVs.
 
         :param width: The width of the plot to be rendered. 
         :param height: The height of the plot to be rendered. 
-        :param xlabel_bottom: The label that will be on the bottom of the plot. 
-        :param xlabel_top: The label that will be on the top of the plot. 
-        :param ylabel_left: The label that will be to the left of the plot. 
-        :param ylabel_right: The label that will be to the right of the plot. 
-        :param xlabel_color_bottom: The color of the x label on the bottom of the plot. 
-        :param xlabel_color_top: The color of the x label on the top of the plot. 
-        :param ylabel_color_left: The color of the y label to the left of the plot. 
-        :param ylabel_color_right: The color of the y label to the right of the plot. 
+        :param xlabel_bottom: The label that will be on the bottom of the plot.
+        :param ylabel_left: The label that will be to the left of the plot.
+        :param xlabel_color: The color of the x label/s.
+        :param ylabel_color: The color of the y label/s.
+        :param title_color: The color of the title.
         :param label_font: The font for all the labels for the plot. 
         :param pv_colors: A list of colors for each pv that is plotted, in the order of df_list. 
-        :param line_types: A list of all line types for each pv that is plotted, in the order of df_list. 
-        :param is_scatter: A boolean for whether or not to plot all points as scatter, or to plot as lines. 
+        :param line_types: A list of all line types for each pv that is plotted, in the order of df_list.
+        :param marker_types: A list of all markers for each pv that is plotted, in the order of df_list.
+        :param is_scatter: A boolean for whether to plot all points as scatter, or to plot as lines.
+        :param is_marker: A boolean for whether to plot all points as markers.
         :param marker_size: The size of the scatter marker, if scatter is chosen as a line_types option. 
-        :param labels: A list of labels for each PV, in the order of df_list. 
-        :param smart_ticks: A boolean for whether or not to display ticks for the time unit that changes, but changes the least. 
-        :param smart_title: A boolean for whether or not to list the PVs in the title. 
+        :param pv_labels: A list of labels for each PV, in the order of df_list.
+        :param num_ticks: The number of ticks along the x-axis of the plot.
+        :param smart_title: A boolean for whether to list the PVs in the title.
         """
 
-        # TODO: program remaining default parameters: 
-        # - xlabel_bottom
-        # - xlabel_top
-        # - ylabel_left
-        # - ylabel_right
-        # - xlabel_color_bottom
-        # - xlabel_color_top
-        # - ylabel_color_left
-        # - ylabel_color_right
-        # - label_font
-        # - pv_colors
-        # - labels
-        # - smart_ticks
+        # LEGEND LABELS
+        if pv_labels is not None:
+            for i in range(len(df_list)):
+                df_curr = df_list[i]
+                df_curr.rename(columns={df_curr.columns[1]: pv_labels[i]}, inplace=True)
 
-        # PLOTTING
-        ax = plt.subplots(figsize=(width, height), layout="constrained") 
-        for i in range(len(df_list)): # plot each DataFrame in df_list
-            df_curr = df_list[i]
-            if not is_scatter: 
-                # choose a line type and plot accordingly
-                curr_line_type = line_types[i % len(line_types)] # cycle through the line_types list 
-                ax.plot(df_curr["timestamps"], df_curr.columns[1], linestyle=curr_line_type, label=df_curr.columns[1], markersize=marker_size)
-            else: 
-                ax.scatter(df_curr["timestamps"], df_curr.columns[1], label=df_curr.columns[1], s=marker_size)
+        fig, ax = plt.subplots(figsize=(width, height), layout="constrained")
 
-        # TICKS
-        if not smart_ticks: 
-            ax.xaxis.set_major_locator(ticker.LinearLocator(5))
-        else: 
-            # TODO: use smart ticks function 
-            pass 
+        # FONTS
+        font_x = {
+            "family": label_font,
+            "color": xlabel_color,
+            "size": 16
+        }
+        font_y = {
+            "family": label_font,
+            "color": ylabel_color,
+            "size": 16
+        }
+        font_title = {
+            "family": label_font,
+            "color": title_color,
+            "size": 20
+        }
 
         # LABELS
-        ax.set_xlabel(xlabel_bottom)
-        ax.set_ylabel(ylabel_left)
-        # set the title 
-        ax.legend()
-        if not smart_title: 
-            ax.set_title("PVs vs. Time")
-        else: 
+        ax.xaxis.set_major_locator(ticker.LinearLocator(num_ticks))
+        ax.yaxis.set_major_locator(ticker.LinearLocator(num_ticks))
+        plt.xlabel(xlabel_bottom, fontdict=font_x)
+        plt.ylabel(ylabel_left, fontdict=font_y)
+
+        # TITLE
+        if not smart_title:
+            plt.title("PVs vs. Time", fontdict=font_title)
+        else:
             # create a title using the PV names
             pv_list = [df_curr.columns[1] for df_curr in df_list]
-            ax.set_title(f"{", ".join(pv_list)} vs. Time")
+            plt.title(f"{", ".join(pv_list)} vs. Time", fontdict=font_title)
 
+        # PLOTTING
+        for i in range(len(df_list)):  # plot each DataFrame in df_list
+            df_curr = df_list[i]
+            if not is_scatter:
+                # choose a line type and plot accordingly
+                curr_line_type = line_types[i % len(line_types)]  # cycle through the line_types list
+                # marker plot
+                if is_marker:
+                    ax.plot(df_curr["timestamps"], df_curr[df_curr.columns[1]], color=pv_colors[i % len(pv_colors)],
+                            linestyle=curr_line_type, label=df_curr.columns[1],
+                            marker=marker_types[i % len(marker_types)], markersize=marker_size)
+                # line plot
+                else:
+                    ax.plot(df_curr["timestamps"], df_curr[df_curr.columns[1]], color=pv_colors[i % len(pv_colors)],
+                            linestyle=curr_line_type, label=df_curr.columns[1])
+            # scatter plot
+            else:
+                ax.scatter(df_curr["timestamps"], df_curr[df_curr.columns[1]], label=df_curr.columns[1], s=marker_size)
+
+        ax.legend()
         plt.show()
         return None
 
-    def plot_correlation(self, 
-                         df: pd.DataFrame, 
-                         pv_x: str, 
-                         pv_y: str, 
-                         start: str, 
-                         end: str) -> None:
+    def plot_correlation(self,
+                         df: pd.DataFrame,
+                         pv_x: str,
+                         pv_y: str,
+                         width=10,
+                         height=7,
+                         xlabel_bottom="Timestamp",
+                         ylabel_left="PV",
+                         xlabel_color="black",
+                         ylabel_color="black",
+                         title_color="black",
+                         label_font="Helvetica",
+                         correl_color="tab:blue",
+                         line_type="solid",
+                         marker_type=".",
+                         is_scatter=True,
+                         is_marker=False,
+                         marker_size=5,
+                         pv_labels=None,
+                         num_ticks=7,
+                         smart_title=False) -> None:
         """Plot the correlation of two PVs. 
 
         :param df: The DataFrame from which the PVs are plotted. 
         :param pv_x: The PV to be plotted on the x-axis. 
-        :param pv_y: The PV to be plotted on the y-axis. 
-        :param start: The start date of the plot in YYYY/MM/DD HH:MM:SS format
-        :param end: The end date of the plot in YYYY/MM/DD HH:MM:SS format
+        :param pv_y: The PV to be plotted on the y-axis.
+
+        :param width: The width of the plot to be rendered.
+        :param height: The height of the plot to be rendered.
+        :param xlabel_bottom: The label that will be on the bottom of the plot.
+        :param ylabel_left: The label that will be to the left of the plot.
+        :param xlabel_color: The color of the x label/s.
+        :param ylabel_color: The color of the y label/s.
+        :param title_color: The color of the title.
+        :param label_font: The font for all the labels for the plot.
+        :param correl_color: The color of the correlation plot.
+        :param line_type: The default line type for the plot.
+        :param marker_type: The default marker type for the plot.
+        :param is_scatter: A boolean for whether to plot all points as scatter, or to plot as lines.
+        :param is_marker: A boolean for whether to plot all points as markers.
+        :param marker_size: The size of the scatter marker, if scatter is chosen as a line_types option.
+        :param pv_labels: A list of labels for each PV, in the order of df_list.
+        :param num_ticks: The number of ticks along the x-axis of the plot.
+        :param smart_title: A boolean for whether to list the PVs in the title.
         """
 
-        # TODO: create and program all default parameters
+        # LEGEND LABELS
+        if pv_labels is not None:
+            df.rename(columns={df.columns[1]: pv_labels[0], df.columns[2]: pv_labels[1]}, inplace=True)
 
-        # plot the correlation 
-        ax = plt.subplots(figsize=(10, 7), layout="constrained")        
-        ax.scatter(df[pv_x], df[pv_y], label=f"{pv_y} vs. {pv_x}", s=3)
-        # tick settings
-        x_ticks = [np.format_float_scientific(i, precision=3, min_digits=2) for i in df[pv_x]]  # scientific notation
-        y_ticks = [np.format_float_scientific(i, precision=3, min_digits=2) for i in df[pv_y]]
-        ax.set_xticklabels(x_ticks, fontsize=10)  # set tick labels
-        ax.set_yticklabels(y_ticks, fontsize=10)
-        ax.xaxis.set_major_locator(ticker.LinearLocator(5))
-        ax.yaxis.set_major_locator(ticker.LinearLocator(5))
-        # plot labels
-        ax.set_xlabel(f"{pv_x}")
-        ax.set_ylabel(f"{pv_y}")
-        ax.set_title(f"{pv_y} vs. {pv_x}")
-        ax.legend()
+        fig, ax = plt.subplots(figsize=(width, height), layout="constrained")
+
+        # FONTS
+        font_x = {
+            "family": label_font,
+            "color": xlabel_color,
+            "size": 16
+        }
+        font_y = {
+            "family": label_font,
+            "color": ylabel_color,
+            "size": 16
+        }
+        font_title = {
+            "family": label_font,
+            "color": title_color,
+            "size": 20
+        }
+
+        # LABELS
+        ax.xaxis.set_major_locator(ticker.LinearLocator(num_ticks))
+        ax.yaxis.set_major_locator(ticker.LinearLocator(num_ticks))
+        plt.xlabel(xlabel_bottom, fontdict=font_x)
+        plt.ylabel(ylabel_left, fontdict=font_y)
+
+        # TITLE
+        if not smart_title:
+            ax.set_title(f"{pv_y} vs. {pv_x}")
+        else:
+            plt.title(f"{pv_labels[0]} vs. {pv_labels[1]}", fontdict=font_title)
+
+        # PLOTTING
+        if not is_scatter:
+            # marker plot
+            if is_marker:
+                ax.plot(df[pv_x], df[pv_y], color=correl_color, linestyle=line_type, marker=marker_type,
+                        markersize=marker_size)
+            # line plot
+            else:
+                ax.plot(df[pv_x], df[pv_y], color=correl_color, linestyle=line_type)
+        # scatter plot
+        else:
+            ax.scatter(df[pv_x], df[pv_y], color=correl_color, s=marker_size)
+
         plt.show()
         return None
--- archive-analysis/lcls-tools-plots/archiver_plotter_test.py
+++ archive-analysis/lcls-tools-plots/archiver_plotter_test.py
@@ -1,17 +1,25 @@
 import archiver_plotter as ap
+archplot = ap.ArchiverPlotter()
 
-# VARIABLES
-aptest = ap.ArchiverPlotter()
-chrg_pv = "TORO:GUNB:360:CHRG"
+# Create variables to represent PVs and datetimes
+xcor_pv = "XCOR:GUNB:293:BACT"
+ycor_pv = "YCOR:GUNB:293:BACT"
 homc1_pv = "SCOP:AMRF:RF01:AI_MEAS1"
 start_str = "2024/07/02 12:00:00"
 end_str = "2024/07/02 13:00:00"
 
-# TESTS: creating DataFrames
-df_chrg = aptest.create_df(chrg_pv, start_str, end_str)
-df_homc1 = aptest.create_df(homc1_pv, start_str, end_str)
-df_correl = aptest.create_correlation_df(df_chrg, df_homc1)
+# Create DataFrames for PVs
+df_xcor = archplot.create_df(xcor_pv, start_str, end_str)
+df_ycor = archplot.create_df(ycor_pv, start_str, end_str)
+df_homc1 = archplot.create_df(homc1_pv, start_str, end_str)
 
-# TESTS: plotting
-aptest.plot_pv_over_time([df_chrg, df_homc1], start_str, end_str)
-aptest.plot_correlation(df_correl, chrg_pv, homc1_pv, start_str, end_str)
+# Create a DataFrame for the correlation between two PVs
+df_correl = archplot.create_correlation_df(df_xcor, df_homc1)
+
+# Plot a list of PVs over time
+archplot.plot_pv_over_time([df_xcor, df_ycor], pv_labels=["XCOR", "YCOR"], ylabel_left="Amplitude (arb. units)")
+
+# TODO: finish testing different plot types with default parameters
+
+# Plot a correlation between two PVs using their correlation DataFrame and String identifiers
+archplot.plot_correlation(df_correl, xcor_pv, homc1_pv)
